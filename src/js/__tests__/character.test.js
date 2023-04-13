ca01999273a038a6cf0438dbaefdf92c3d6f7277
Andrey Shestakov <andrey230781.an@gmail.com>
2023-04-13 12:57:56 +0300

    change tests for type

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -47,12 +47,12 @@ test('Получим ошибку имени', () => {
 
 test('Получим ошибку типа имени', () => {
     const description = () => new Character(15, 'nsw');
-    expect(description).toThrow('Name and/or Type should be string');
+    expect(description).toThrow('Name should be string');
 });
 
 test('Получим ошибку типа', () => {
     const description = () => new Character('Nick', 15);
-    expect(description).toThrow('Name and/or Type should be string');
+    expect(description).toThrow('Type should be string');
 });
 
 test('Получим ошибку типа2', () => {
